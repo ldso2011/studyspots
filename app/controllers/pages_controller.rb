7e8218ec41d4aa629173ea30b5811b2e3997e746
ruben-veloso <ruben-veloso@hotmail.com>
2011-11-14 16:55:54 +0000

    Primeira versão, com devise a funcionar.
    Signed-off-by: ruben-veloso <ruben-veloso@hotmail.com>

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def privacy
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudySpots::Application.config.secret_token = 'e35e799dd4ab2dcb505d68c6ef330b65f828606cd5f34e44f0f234dde9d8c0dbea2879a7de968d76c7f4637b63a3904099773907ba52964b0eadea41f587a21e'
