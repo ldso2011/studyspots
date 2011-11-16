49acb92a3fbcf9a9e7ee098fb6a195e7a6aeb26b
Ricardo Teixeira <ricardo@ctrler.eu>
2011-11-16 19:28:50 +0000

    Alterei URL do mail de producao para quiet-mist-9354.heroku.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ StudySpots::Application.configure do
   config.active_support.deprecation = :notify
   
   
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'quiet-mist-9354.heroku.com' }
 
   config.action_mailer.delivery_method = :smtp
   
