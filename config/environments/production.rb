f00d9ebd3f74636ddd76ebf5b355586b0089cfc8
Ricardo Teixeira <ricardo@ctrler.eu>
2011-11-16 15:58:14 +0000

    Adicionei conta de email a ambiente de producao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,17 @@ StudySpots::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'ldso2011@gmail.com',
+    :password             => 'feupmieic2011',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
