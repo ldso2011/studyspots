203e5422113ba4ad11b01af70b517bea1e828964
Rúben <ruben-veloso@hotmail.com>
2011-12-19 22:53:08 +0000

    - Test Units clean; - Test Units Faculty;

--- test/unit/course_test.rb
+++ test/unit/course_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Insert_Course" do
+    course = Course.new
+    course.acronym = "MIEIC"
+    course.faculty_id = faculties(:FEUP).faculty_id
+    course.name = "Mestrado Integrado em Engenharia Informatica e Computacao"
+    assert !course.save
+  end
 end
--- test/unit/faculty_test.rb
+++ test/unit/faculty_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class FacultyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Insert_Faculty" do
+    faculty = Faculty.new
+    faculty.faculty_id = UUIDTools::UUID.timestamp_create().to_s
+    faculty.acronym = "FEUP"
+    faculty.faculty_type = 0
+    faculty.name = "Faculdade de Engenharia da Universidade do Porto"
+    faculty.url = "http://www.fe.up.pt/"
+    assert faculty.save, "Devia inserir com sucesso!"
+  end
+  test "Dont_Insert_Faculty_Without_Id" do
+    faculty = Faculty.new
+    faculty.acronym = "FEUP"
+    faculty.faculty_type = 0
+    faculty.name = "Faculdade de Engenharia da Universidade do Porto"
+    faculty.url = "http://www.fe.up.pt/"
+    assert !faculty.save, "Nao devia dar sucesso sem id!"
+  end
 end
