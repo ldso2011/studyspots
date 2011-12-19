942a9b4d3f8c2c8ae4adc52ab4b560700a29b000
Rúben <ruben-veloso@hotmail.com>
2011-12-19 13:39:56 +0000

    - Faltava quebras de linhas; - Auto redirecciona para o create profile senão existir;

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!, :except => [:home, :contact, :privacy, :about]
+  before_filter :authenticate_user!, :except => [:home, :contact, :privacy, :about], :create_user_profile
 
   def home
   end
@@ -14,5 +14,12 @@ class PagesController < ApplicationController
   end
 
   def about
+  end
+  
+   private
+  def create_user_profile
+    if Profile.find_by_user_id(current_user.id) == nil
+      redirect_to users_profile_index_path
+    end
   end
 end
